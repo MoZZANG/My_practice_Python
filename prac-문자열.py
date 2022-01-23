76ba22c23a083bb0fc0c810e10e206ca6da379af
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-01-23 10:07:36 +0900

    Add files via upload

---
+++ prac-문자열.py
@@ -0,0 +1,22 @@
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace('Python', 'Java'))
+
+# index = python.index('n')
+# print(index)
+# index = python.index('n', index + 1)
+# print(index)
+
+# print(python.find('n'))
+# print(python.count('n'))
+
+
+print('나는 %s살이고 %s색을 좋아해요.' % (20, '빨간'))
+print('나는 {}살이고 {}색을 좋아해요.'.format(20, '빨간'))
+print('나는 {1}살이고 {0}색을 좋아해요.'.format('빨간', 20))
+age = 20
+color = '빨간'
+print(f'나는 {age}살이고 {color}색을 좋아해요.')
