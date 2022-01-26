391ac555a8e0f2ea9160fe88dc2cbec1f08bd0d3
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-01-26 12:06:06 +0900

    Add files via upload

---
+++ prac_for(반복문).py
@@ -0,0 +1,7 @@
+# for waiting_no in range(1, 11):
+#     print('대기번호 : {}'.format(waiting_no))
+
+
+Starbucks = ['김태희', '전지현', '이보영']
+for customer in Starbucks:
+    print("{}님, 주문하신 메뉴나왔습니다.".format(customer))
