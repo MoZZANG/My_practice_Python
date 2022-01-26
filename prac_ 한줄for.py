f1e06c74a8360d9370eb5b24a82038df5a76aa54
MoZZANG <98200337+MoZZANG@users.noreply.github.com>
2022-01-26 12:08:24 +0900

    Add files via upload

---
+++ prac_ 한줄for.py
@@ -0,0 +1,11 @@
+# students = [1, 2, 3, 4, 5]
+# students = [i+100 for i in students]
+# print(students)
+
+Heroes = ['iron man', 'thor', 'super man']
+Heroes = [len(i) for i in Heroes]
+print(Heroes)
+
+Heroes = ['iron man', 'thor', 'super man']
+Heroes = [i.upper() for i in Heroes]
+print(Heroes)
